5ee063d5cbabc96fe3302bb662cfd232ec270f37
Prachi Jain <prachijain@Prachis-MacBook-Air.local>
2025-10-06 17:21:38 +0530

    website changes done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,24 +11,6 @@ import Head from "next/head";
 export default function HomePage() {
   return (
     <>
-    <Head>
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              "@context": "https://schema.org",
-              "@type": "Organization",
-              "name": "Kinetara",
-              "url": "https://www.kinetaraphysiotherapy.com",
-              "logo": "https://www.kinetaraphysiotherapy.com/android-chrome.png",
-              "sameAs": [
-                    "https://www.facebook.com/kinetaraphysiotherapy/",
-                    "https://www.instagram.com/kinetara.physiotherapy/",
-              ]
-            })
-          }}
-        />
-      </Head>
     <main>
       <HeroSection />
       <OfferingsSection />
