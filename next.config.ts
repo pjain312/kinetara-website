e952f4f689db900e69b6173d426748f24aa1918d
Prachi Jain <prachijain@Prachis-MacBook-Air.local>
2025-10-07 21:45:06 +0530

    optimized data

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.kinetaraphysiotherapy.com' }],
+        destination: 'https://kinetaraphysiotherapy.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
